893a0de0ad1e49d80e7f20056091a5a885a0212b
spmno <sunqingpeng@hotmail.com>
2025-07-24 13:46:48 +0800

    Update build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,4 @@
-use std::env;
-
 fn main() {
     // Set necessary environment variables for WebKit to prevent initialization errors
-    println!("cargo:rustc-env=WEBKIT_DISABLE_DMABUF_RENDERER=1");
-    
     tauri_build::build()
 }
